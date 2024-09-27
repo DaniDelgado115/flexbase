711e60a143ffebeae8d25cb81475fb3da02de049
Daniel Delgado Eceiza <158839759+DaniDelgado115@users.noreply.github.com>
2024-09-27 15:05:19 +0000

    rrw

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-window.onload(){
+window.onload=()=>{
   let datubase=[
     {
       "isbn": "1617293563",
       "egilea": "Raoul-Gabriel Urma",
       "data": "Nov 15, 2018",
       "izenburua": "Modern Java in Action: Lambdas, streams, functional and reactive programming",
-      "filename": "https://covers.openlibrary.org/lb/id/8508261-L.jpg"
+      "filename": "https://covers.openlibrary.org/lb/id/8508261-M.jpg"
     },
   
    {
@@ -13,7 +13,7 @@ window.onload(){
       "egilea": "Benjamin Muschko",
       "data": "Mar 09, 2014",
       "izenburua": "Gradle in Action",
-      "filename": "https://covers.openlibrary.org/lb/id/8514400-L.jpg"
+      "filename": "https://covers.openlibrary.org/lb/id/8514400-M.jpg"
     },
   
        {
@@ -21,7 +21,7 @@ window.onload(){
       "egilea": "Matt Welsh",
       "data": "1995",
       "izenburua": "The Linux bible",
-      "filename": "https://covers.openlibrary.org/lb/id/6764181-L.jpg"
+      "filename": "https://covers.openlibrary.org/lb/id/6764181-M.jpg"
     },
     
   {
@@ -29,9 +29,32 @@ window.onload(){
       "egilea": "Dmitry Jemerov",
       "data": "Feb 19, 2017",
       "izenburua": "Kotlin in Action",
-      "filename": "https://covers.openlibrary.org/lb/id/8507716-L.jpg"
+      "filename": "https://covers.openlibrary.org/lb/id/8507716-M.jpg"
     }
   ]
-document.querySelector('body > div > div.flex.left > img').src=datubase[1].filename
+  let ezkb=document.getElementById("ezk")
+  let eskb=document.getElementById("esk")
+
+  let indizea=0;
+
+  eguneraketa = (i) =>{ 
+    document.getElementById('irudia').src=datubase[i].filename
+    document.getElementById('izenb').value=datubase[i].izenburua
+    document.getElementById('eg').value=datubase[i].egilea
+    document.getElementById('isbn').value=datubase[i].isbn
+    document.getElementById('data').value=datubase[i].data    
+  }
+  ezkb.onclick=() =>{
+    indizea--;
+    if (indizea<0) indizea=datubase.length-1
+    eguneraketa(indizea)
+  }
+  eskb.onclick=() =>{
+    indizea++;
+    if (indizea>datubase.length-1) indizea=0;
+    eguneraketa(indizea)
+  }
+
+  //document.getElementById('irudia').src=datubase[1].filename
 }
 
